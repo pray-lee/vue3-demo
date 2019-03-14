9a6134cd3a40884afb2f32215e34d203604c2a10
pray-lee <praycis@gmail.com>
2019-03-14 22:23:14 +0800

    update mock

--- src/mock/response/userInfo.js
+++ src/mock/response/userInfo.js
@@ -1,6 +1,5 @@
 import Mock from 'mockjs'
 export const userInfo = (options) => {
-  console.log(options)
   const template = {
     // 这里就是mock的模拟数据生成规, 规则地址： https://github.com/nuysoft/Mock/wiki/Syntax-Specification
   }
